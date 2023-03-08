fe8be5cdfda7118d28badc99a97c9374d5a43014
aditiakusuma <aditiakusuma800@gmail.com>
2023-03-08 14:26:27 +0700

    add router

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 
 // import './assets/main.css'
 import './assets/tailwind.css'
 
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    
+  ]
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(router)
 
 app.mount('#app')

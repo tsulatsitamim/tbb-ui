a2433bf69826f2fcab76305b5ebf40d45abdea2f
aditiakusuma <aditiakusuma800@gmail.com>
2023-03-08 14:44:07 +0700

    add bg

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,10 @@ import SideBar from './components/SideBar.vue'
 </script>
 
 <template>
-  <SideBar />
+  <div class="bg-secondary">
+    <SideBar />
+    <div class="w-full h-screen"></div>
+  </div>
 </template>
 
 <style scoped></style>

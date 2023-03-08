5f02b3b92e478abab4eb44fe91818653183c6981
aditiakusuma <aditiakusuma800@gmail.com>
2023-03-08 15:51:50 +0700

    add font

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 
-// import './assets/main.css'
+import './assets/main.css'
 import './assets/tailwind.css'
 
 const router = createRouter({

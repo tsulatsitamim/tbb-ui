dcee8c4fe385b4655e916e37d565ea7c2cd7fa63
aditiakusuma <aditiakusuma800@gmail.com>
2023-03-11 16:07:44 +0700

    add variant

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,13 @@ module.exports = {
       colors: {
         primary: '#5867dd',
         secondary: '#e4e6ef',
+        success: '#1bc5bd',
+        danger: '#f64e60',
+        warning: '#ffa800',
+        info: '#8950fc',
+        dark: '#181c32',
+        light: '#f3f6f9',
+        muted: '#b5b5c3'
       },
     },
   },

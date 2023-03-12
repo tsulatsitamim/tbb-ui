105ea514dc8cbf4478a688a07b144728a83ff8a7
Tsulatsi Tamim <tsulatsitamim@gmail.com>
2023-03-12 21:17:44 +0700

    improve spinner position

--- src/components/button/AppButton.vue
+++ src/components/button/AppButton.vue
@@ -23,10 +23,10 @@ defineEmits(['click'])
 <template>
   <button class="rounded-md hover:brightness-90 font-semibold inline-flex items-center" :class="`${colors[color]} ${(loading || disabled) && 'opacity-60 cursor-not-allowed disabled'
     } ${compact ? 'px-3 py-2 text-sm' : 'px-5 py-3 '}`" :disabled="!!loading" @click="$emit('click')">
-    <div class="relative" :class="`${loading && 'pl-5'}`">
+    <div class="relative flex items-center" :class="`${loading && 'pl-5'}`">
       <!-- TODO: SVG Loading For Campact -->
-      <svg v-if="loading" class="animate-spin h-5 w-5 text-white absolute -left-1 top-[2px]"
-        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+      <svg v-if="loading" class="animate-spin h-5 w-5 text-white absolute -left-1" xmlns="http://www.w3.org/2000/svg"
+        fill="none" viewBox="0 0 24 24">
         <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
         <path class="opacity-75" fill="currentColor"
           d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">

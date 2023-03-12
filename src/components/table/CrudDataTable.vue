4646b8c7c72207cf13c48039ef5882ac36bc8631
Tsulatsi Tamim <tsulatsitamim@gmail.com>
2023-03-12 16:53:51 +0700

    add crudtable

---
+++ src/components/table/CrudDataTable.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import type { DTable } from './DataTable.spec'
+import type { $Fetch } from "ofetch";
+import { ofetch } from "ofetch";
+import DataTable from './DataTable.vue';
+import { ref } from 'vue';
+
+const props = withDefaults(defineProps<{
+  table: DTable,
+  ofetch?: $Fetch
+}>(), {
+  ofetch() {
+    return ofetch
+  }
+})
+
+
+const loading = ref(false)
+
+const deleteItem = async (id: string | number) => {
+  if (!window.confirm('Hapus item?')) {
+    return
+  }
+
+  loading.value = true
+  try {
+    await props.ofetch(`${props.table.url}/${id}`, { method: "DELETE" })
+  } catch (error) {
+    console.log(error);
+
+    alert('Mohon maaf ada gangguan sistem.')
+  }
+  loading.value = false
+}
+</script>
+
+<template>
+  <DataTable :table="table">
+    <template #column(action)="{ data }">
+      <div class="flex justify-center">
+        <NuxtLink :to="`/tahun-ajaran/${data.id}`"
+          class="text-xs font-bold text-slate-500 shadow-none mr-3 hover:opacity-75">
+          EDIT
+        </NuxtLink>
+        <button class="text-xs font-bold text-slate-500 shadow-none hover:opacity-75" @click="deleteItem(data.id)">
+          HAPUS
+        </button>
+      </div>
+    </template>
+  </DataTable>
+</template>

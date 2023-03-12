c90c7c2e19d83cc77edd4b2940e00c93f72af86e
Tsulatsi Tamim <tsulatsitamim@gmail.com>
2023-03-12 17:54:47 +0700

    add basic card and input

---
+++ src/components/AppCard.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="bg-white pt-5 pb-6 px-6 lg:pt-6 lg:pb-10 lg:px-7 rounded-md">
+        <slot></slot>
+    </div>
+</template>
---
+++ src/components/form/FormInput.vue
@@ -0,0 +1,41 @@
+<script setup>
+defineProps({
+  modelValue: { type: [String, Number, File], default: '' },
+  label: { type: String, default: '' },
+  placeholder: { type: String, default: '' },
+  type: { type: String, default: 'text' },
+  error: { type: String, default: '' },
+  help: { type: String, default: '' },
+})
+
+defineEmits(['modelValue'])
+</script>
+
+<template>
+  <div>
+    <label class="mb-2 ml-1 text-slate-700 block">
+      {{ label }}
+    </label>
+    <div>
+      <input v-if="type !== 'file'" :value="value" :type="type"
+        class="bg-white border border-gray-300 text-gray-900 rounded-lg focus:outline-none focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        :class="{
+          'border-red-500 placeholder-red-500 focus:ring-red-500 focus:border-red-500':
+            error,
+        }" :placeholder="placeholder || `Masukan ${label || 'Input'}`"
+        @input="$emit('modelValue', $event.target.value)" />
+      <input v-else type="file"
+        class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:outline-none focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        :class="{
+          'border-red-500 placeholder-red-500 focus:ring-red-500 focus:border-red-500':
+            error,
+        }" @input="$emit('modelValue', $event.target.files[0])" />
+      <p v-if="error" class="mt-2 text-sm text-red-600">
+        {{ error }}
+      </p>
+      <p v-if="help" class="mt-2 text-sm text-gray-400">
+        {{ help }}
+      </p>
+    </div>
+  </div>
+</template>

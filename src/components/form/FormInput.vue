5d25b06b30eddf9456fdaa7ed5c02255bf2eafbf
Tsulatsi Tamim <tsulatsitamim@gmail.com>
2023-03-13 08:10:24 +0700

    add form select & improve ux

--- src/components/form/FormInput.vue
+++ src/components/form/FormInput.vue
@@ -13,19 +13,19 @@ defineEmits(['update:modelValue'])
 
 <template>
   <div>
-    <label class="mb-2 ml-1 text-slate-700 block">
+    <label class="mb-1 ml-1 text-slate-700 block">
       {{ label }}
     </label>
     <div>
       <input v-if="type !== 'file'" :value="modelValue" :type="type"
-        class="bg-white border border-gray-300 text-gray-900 rounded-lg focus:outline-none focus:ring-blue-500 focus:border-blue-500 block w-full px-3 py-2"
+        class="bg-white border border-gray-300 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 block w-full px-3 py-2"
         :class="{
           'border-red-500 placeholder-red-500 focus:ring-red-500 focus:border-red-500':
             error,
         }" :placeholder="placeholder || `Masukan ${label || 'Input'}`"
         @input="$emit('update:modelValue', $event.target.value)" />
       <input v-else type="file"
-        class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:outline-none focus:ring-blue-500 focus:border-blue-500 block w-full px-3 py-2"
+        class="bg-gray-50 border border-gray-300 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 block w-full px-3 py-2"
         :class="{
           'border-red-500 placeholder-red-500 focus:ring-red-500 focus:border-red-500':
             error,
---
+++ src/components/form/FormSelect.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+withDefaults(defineProps<{
+  modelValue?: string | number | null
+  label?: string
+  placeholder?: string
+  items: any[]
+  disabled?: boolean | string | number
+  error?: string
+}>(), {
+  modelValue: '',
+  label: '',
+  placeholder: '',
+  disabled: false,
+  error: '',
+})
+
+const localValue = ref('')
+
+const emit = defineEmits(['update:modelValue'])
+const update = (e: any) => {
+  console.log(e);
+
+  emit('update:modelValue', e.target.value)
+  localValue.value = e.target.value
+}
+
+</script>
+
+<template>
+  <div class="w-full">
+    <label v-if="label" class="mb-1 ml-1 text-slate-700 block">{{ label }}</label>
+    <div>
+      <div class="select-wrapper relative">
+        <select :value="modelValue"
+          class="appearance-none border border-gray-300 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 block w-full pl-3 pr-6 py-2"
+          :class="`${modelValue || localValue ? 'text-gray-700' : 'text-slate-300'} ${disabled ? 'bg-gray-100' : 'bg-white'
+            } ${error &&
+            'text-red-500 border-red-500 placeholder-red-700 focus:ring-red-500 focus:border-red-500'
+            }`" :disabled="!!disabled" @input="update">
+          <option value="" disabled selected>
+            {{ placeholder || `Pilih ${label || 'Item'}` }}
+          </option>
+          <option v-for="item in items" :key="item.id" :value="item.id"
+            :selected="item.id == modelValue || item.id == localValue">
+            {{ item.name }}
+          </option>
+        </select>
+        <p v-if="error" class="mt-2 text-sm text-red-600 dark:text-red-500">
+          {{ error }}
+        </p>
+
+        <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true" class="h-4 absolute right-1 top-3">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5"></path>
+        </svg>
+      </div>
+    </div>
+  </div>
+</template>
+

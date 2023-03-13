08edc53f473b2363c39d277501df161b7613ba9b
aditiakusuma <aditiakusuma800@gmail.com>
2023-03-13 16:27:32 +0700

    add toggle function

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,26 +1,36 @@
 <script setup lang="ts">
-import SideBar from './SideBar.vue';
-import HeaderMobile from './HeaderMobile.vue';
-import Header from './Header.vue';
-import Footer from './Footer.vue';
-import { storeToRefs } from 'pinia';
+import SideBar from './SideBar.vue'
+import HeaderMobile from './HeaderMobile.vue'
+import Header from './Header.vue'
+import Footer from './Footer.vue'
+import { storeToRefs } from 'pinia'
 
-import { useSidebarStore } from '../stores/sidebar';
+import { useSidebarStore } from '@/stores/sidebar'
 
 const sidebarStore = useSidebarStore()
 
-const { isExpandSidebar } = storeToRefs(sidebarStore)
+const { hideSidebar } = sidebarStore
+
+const { isExpandSidebar, isShowSidebarMobile } = storeToRefs(sidebarStore)
 </script>
 
 <template>
   <div class="min-h-screen">
     <HeaderMobile></HeaderMobile>
-    <div class="flex flex-row flex-[1_0_auto] h-full">
+    <div class="flex flex-row flex-[1_0_auto] h-full relative">
+      <div
+        v-if="isShowSidebarMobile"
+        id="layer-black"
+        class="absolute md:hidden w-screen z-[1000] h-screen bg-black/30"
+        @click="hideSidebar"
+      ></div>
       <SideBar></SideBar>
-      <div class="flex flex-col justify-between flex-1 pt-[65px] min-w-0 min-h-screen"
-        :class="isExpandSidebar ? 'pl-[265px]' : 'pl-[79px]'">
+      <div
+        class="flex flex-col justify-between flex-1 pt-[65px] min-w-0 min-h-screen"
+        :class="isExpandSidebar ? 'md:pl-[265px]' : 'md:pl-[79px]'"
+      >
         <Header></Header>
-        <div class=" p-4">Test</div>
+        <div class="p-4">Test</div>
         <!-- <router-view :key="$route.fullPath" /> -->
         <Footer></Footer>
       </div>

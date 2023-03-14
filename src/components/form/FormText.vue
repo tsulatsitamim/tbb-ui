beed7cf4446c4cbc4306c99544de69b0a2607e68
Tsulatsi Tamim <tsulatsitamim@gmail.com>
2023-03-14 10:42:13 +0700

    add textarea

---
+++ src/components/form/FormText.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+withDefaults(defineProps<{
+  modelValue?: string | null
+  label: string
+  placeholder?: string
+  error?: string
+  help?: string
+  rows?: number | string
+}>(), {
+  rows: 3
+})
+
+defineEmits(['update:modelValue'])
+</script>
+
+<template>
+  <div>
+    <label v-if="label" class="mb-2 ml-1 text-slate-700 block">
+      {{ label }}
+    </label>
+    <div>
+      <textarea :value="modelValue as any"
+        class="bg-white border border-gray-300 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        :placeholder="placeholder || `Masukan ${label || 'Input'}`" :class="{
+          'border-red-500 placeholder-red-500 focus:ring-red-500 focus:border-red-500':
+            error,
+        }" :rows="rows" @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)" />
+      <p v-if="error" class="mt-1 text-sm text-red-600 dark:text-red-500">
+        {{ error }}
+      </p>
+      <p v-if="help" class="mt-1 text-sm text-gray-400">
+        {{ help }}
+      </p>
+    </div>
+  </div>
+</template>

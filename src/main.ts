2ec5667d9732587e4b6a8278a2a93a9c943e4a10
Tsulatsi Tamim <tsulatsitamim@gmail.com>
2023-03-25 06:52:51 +0700

    chore(package.json): update version from 0.0.1 to 1.0.0 style(main.css): add background color and font size to body feat(main.ts): import tailwind.css file in main.ts file

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 
+import './assets/tailwind.css'
 import './assets/main.css'
 
 const router = createRouter({

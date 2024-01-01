37be883bfaf0794936cad263d20989fa785d2b60
Tsulatsi Tamim <tsulatsitamim@gmail.com>
2024-01-01 15:53:48 +0700

    add disabled

--- src/components/form/FormText.vue
+++ src/components/form/FormText.vue
@@ -6,8 +6,10 @@ withDefaults(defineProps<{
   error?: string
   help?: string
   rows?: number | string
+  disabled?: boolean
 }>(), {
-  rows: 3
+  rows: 3,
+  disabled: false
 })
 
 defineEmits(['update:modelValue'])
@@ -20,11 +22,14 @@ defineEmits(['update:modelValue'])
     </label>
     <div>
       <textarea :value="modelValue as any"
-        class="bg-white border border-gray-300 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        class="border border-gray-300 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
         :placeholder="placeholder || `Masukan ${label || 'Input'}`" :class="{
-            'border-red-500 placeholder-red-500 focus:ring-red-500 focus:border-red-500':
-              error,
-          }" :rows="rows" @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)" />
+          'border-red-500 placeholder-red-500 focus:ring-red-500 focus:border-red-500':
+            error,
+          'cursor-not-allowed bg-slate-100': disabled,
+          'bg-white ': !disabled
+        }" :rows="rows" @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+        :disabled="disabled" />
       <p v-if="error" class="mt-1 text-xs text-red-600 dark:text-red-500">
         {{ error }}
       </p>
